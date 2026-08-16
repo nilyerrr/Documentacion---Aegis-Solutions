e8489247c49410ba179754564a678deda096cedf
Reylin Santana <reylinsantana19@gmail.com>
2026-08-15 20:17:47 -0400

    vault backup: 2026-08-15 20:17:47

--- Proyecto Final - Empresa 1/website/vite.config.js
+++ Proyecto Final - Empresa 1/website/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
   server: {
     host: true,
     allowedHosts: [
